e30e4900a4e60b008d2db09a800c9312cd4442da
Mariusz Nowak <mariusz@medikoo.com>
2014-04-13 11:45:41 +0200

    Update up to changes in es5-ext

--- valid-timeout.js
+++ valid-timeout.js
@@ -1,10 +1,10 @@
 'use strict';
 
-var toUint   = require('es5-ext/number/to-uint')
+var toPosInt = require('es5-ext/number/to-pos-integer')
   , maxValue = require('./max-value');
 
 module.exports = function (value) {
-	value = toUint(value);
+	value = toPosInt(value);
 	if (value > maxValue) {
 		throw new TypeError(value + " is to large for a timeout");
 	}
