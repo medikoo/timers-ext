b649777db7e124d0a60a0a694f82b371bc11e46f
Mariusz Nowak <medyk@medikoo.com>
2018-03-07 17:18:23 +0100

    test: increase time gaps to reduce race conditions

--- test/throttle.js
+++ test/throttle.js
@@ -4,38 +4,38 @@ module.exports = function (t, a, d) {
 	var called = 0;
 	var fn = t(function () {
 		++called;
-	}, 50);
+	}, 100);
 
 	fn();
 	a(called, 1);
 	fn();
 	fn();
 	a(called, 1);
-	// Wait 30ms
+	// Wait 60ms
 	setTimeout(function () {
 		a(called, 1);
 		fn();
-		// Wait 30ms
+		// Wait 60ms
 		setTimeout(function () {
 			a(called, 2);
 			fn();
 			fn();
 
-			// Wait 20ms
+			// Wait 40ms
 			setTimeout(function () {
 				a(called, 2);
 
-				// Wait 30ms
+				// Wait 60ms
 				setTimeout(function () {
 					a(called, 3);
 
-					// Wait 100ms
+					// Wait 200ms
 					setTimeout(function () {
 						a(called, 3);
 						d();
-					}, 100);
-				}, 30);
-			}, 20);
-		}, 30);
-	}, 30);
+					}, 200);
+				}, 60);
+			}, 40);
+		}, 60);
+	}, 60);
 };
